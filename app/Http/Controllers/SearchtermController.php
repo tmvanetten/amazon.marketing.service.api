c170cfffe25403d358cb5ca3f46799a4ba091503
jorge m04 <jorge@all4cellular.com>
2017-03-28 10:05:13 -0400

    3/282017

---
+++ app/Http/Controllers/SearchtermController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Model\Searchterm;
+use App\Model\SearchtermUploadException;
+
+class SearchtermController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * get search terms data
+     *
+     * @return json response
+     */
+    public function get(Request $request) {
+        $result = [
+          'searchterms' => [],
+          'count' => 0
+        ];
+        $skip = $request->input('skip');
+        $rows = $request->input('rows');
+        $campaingName = $request->input('campaingName');
+        $adGroupName = $request->input('adGroupName');
+        $keyWordName = $request->input('keyWordName');
+        $matchType = $request->input('matchType');
+        try{
+            if($campaingName || $adGroupName || $keyWordName) {
+                $wheres = [];
+                if($campaingName)
+                    $wheres['campaign_name'] = $campaingName;
+                if($adGroupName)
+                    $wheres['adgroup_name'] = $adGroupName;
+                if($keyWordName)
+                    $wheres['keyworod'] = $keyWordName;
+                if($matchType)
+                    $wheres['match_type'] = $matchType;
+                $result['searchterms'] = Searchterm::where($wheres)->offset($skip)->limit($rows)->get();
+                $result['count'] = Searchterm::where($wheres)->count();
+            } else {
+                $result['searchterms'] = Searchterm::take($rows)->offset($skip)->get();
+                $result['count'] = count(Searchterm::all());
+            }
+        }catch (\Exception $e) {
+            return response()->json(['errors' => [$e->getMessage()]], 422);
+        }
+        return response()->json(['data' => $result], 200);
+    }
+
+    /**
+     * upload search terms data
+     *
+     * @return json response
+     */
+    public function upload(Request $request) {
+
+        $files = $this->parse_raw_http_request();
+        $uploadedFileString = '';
+        foreach($files as $name => $file) {
+            $uploadedFileString = $file;
+            break;
+        }
+        try{
+            Searchterm::truncate();
+            $searchTerm = new Searchterm();
+            $searchTerm->upload($uploadedFileString);
+        }catch (SearchtermUploadException $e){
+            return response()->json(['errors' => [$e->getMessage()]], 422);
+        }catch (\Exception $e) {
+            return response()->json(['errors' => [$e->getMessage()]], 422);
+        }
+        return response()->json('{}', 200);
+    }
+
+    public function parse_raw_http_request()
+    {
+        $a_data = [];
+        // read incoming data
+        $input = file_get_contents('php://input');
+
+        // grab multipart boundary from content type header
+        preg_match('/boundary=(.*)$/', $_SERVER['CONTENT_TYPE'], $matches);
+        $boundary = $matches[1];
+
+        // split content by boundary and get rid of last -- element
+        $a_blocks = preg_split("/-+$boundary/", $input);
+        array_pop($a_blocks);
+
+        // loop data blocks
+        foreach ($a_blocks as $id => $block)
+        {
+            if (empty($block))
+                continue;
+
+            // you'll have to var_dump $block to understand this and maybe replace \n or \r with a visibile char
+
+            // parse uploaded files
+            if (strpos($block, 'application/octet-stream') !== FALSE)
+            {
+                // match "name", then everything after "stream" (optional) except for prepending newlines
+                preg_match("/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s", $block, $matches);
+            }
+            // parse all other fields
+            else
+            {
+                // match "name" and optional value in between newline sequences
+                preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
+            }
+            $a_data[$matches[1]] = $matches[2];
+        }
+        return $a_data;
+    }
+}
---
+++ app/Model/Searchterm.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use \Exception;
+use DB;
+
+class Searchterm extends Model
+{
+    const ACCEPTED_FORMAT = "Content-Type: text/plain";
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'campaign_name', 'adgroup_name', 'customer_search_term', 'keyworod', 'match_type', 'impressions', 'clicks', 'ctr', 'spend', 'cpc', 'acos'
+    ];
+
+    protected $dataFields = [
+        'Campaign Name' => [
+            'code' => 'campaign_name'
+        ],
+        'Ad Group Name' => [
+            'code' => 'adgroup_name'
+        ],
+        'Customer Search Term' => [
+            'code' => 'customer_search_term'
+        ],
+        'Keyword' => [
+            'code' => 'keyworod'
+        ],
+        'Match Type' => [
+            'code' => 'match_type'
+        ],
+        'Impressions' => [
+            'code' =>'impressions'
+        ],
+        'Clicks' => [
+            'code' =>'clicks'
+        ],
+        'CTR' => [
+            'code' =>'ctr'
+        ],
+        'Total Spend' => [
+            'code' =>'spend'
+        ],
+        'Average CPC' => [
+            'code' =>'cpc'
+        ],
+        'ACoS' => [
+            'code' =>'acos'
+        ]
+    ];
+
+    protected $table = 'search_terms_report';
+
+    public function upload($file) {
+        $data = [];
+        $dataToInsert = [];
+        $lines = explode(PHP_EOL, $file);
+        if(trim($lines[0]) != self::ACCEPTED_FORMAT) {
+            throw new SearchtermUploadException('Incorrect file type, only Amazon report text file type is allowed!');
+        }
+        //unset file type line
+        unset($lines[0]);
+        //unset empty second line
+        unset($lines[1]);
+
+        foreach ($lines as $line) {
+            $data[] = explode("\t", $line);
+        }
+        //imported data fields
+        $dataFields = $data[0];
+        //required data fields
+        $fields = $this->dataFields;
+        foreach($fields as $label => $field) {
+            $index = array_search($label, $dataFields);
+            if($index === false)
+                throw new SearchtermUploadException("Field $label is missing, please double check your file!");
+            $fields[$label]['index'] = $index;
+        }
+        //unset title field
+        unset($data[0]);
+        //prepare insert Data
+        foreach($data as $index => $item) {
+            //if($index>10)
+            //    continue;
+            try{
+                $lineToInsert = [];
+                foreach($fields as $label => $field) {
+                    $fieldValue = $item[$field['index']];
+                    if($field['code']=='ctr' || $field['code']=='acos' )
+                        $fieldValue = rtrim($fieldValue, '%');
+                    $lineToInsert[$field['code']] = $fieldValue;
+                }
+                $dataToInsert[] = $lineToInsert;
+            } catch (Exception $e){
+
+            }
+        }
+
+        $datasToInsert = array_chunk($dataToInsert, 5000);
+        foreach($datasToInsert as $dataToInsert) {
+            $this->bulkInsert($dataToInsert);
+        }
+    }
+
+    protected function bulkInsert($data) {
+        $pdo = DB::connection()->getPdo();
+        $pdo->beginTransaction(); // also helps speed up your inserts.
+        $datafields = [];
+        $fields = $this->dataFields;
+        foreach($fields as $field) {
+            $datafields[] = $field['code'];
+        }
+
+        $insert_values= [];
+        foreach($data as $d){
+            $question_marks[] = '('  . $this->placeholders('?', sizeof($d)) . ')';
+            $insert_values = array_merge($insert_values, array_values($d));
+        }
+
+        $sql = "INSERT INTO search_terms_report (" . implode(",", $datafields ) . ") VALUES " . implode(',', $question_marks);
+
+        $stmt = $pdo->prepare($sql);
+        try {
+            $stmt->execute($insert_values);
+        } catch (PDOException $e){
+            throw new SearchtermUploadException($e->getMessage());
+        }
+        $pdo->commit();
+    }
+
+    protected function placeholders($text, $count=0, $separator=","){
+        $result = array();
+        if($count > 0){
+            for($x=0; $x<$count; $x++){
+                $result[] = $text;
+            }
+        }
+
+        return implode($separator, $result);
+    }
+}
+
+class SearchtermUploadException extends Exception{
+
+}
